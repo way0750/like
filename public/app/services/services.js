e4353706896d1447847024d562db343fe959d995
Yifeng Wang <yeefom@gmail.com>
2015-12-08 16:01:16 -0800

    Add browseService

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -1,31 +1,34 @@
-angular.module('like.service', ['like'])
-.factory('authService', function ($http) {
-  var login = function (userObj) {
-   $http({
-    method : 'post',
-    url : '/api/login',
-    data : userObj
-   });
-  };
-  return {login: login};
-})
-.factory('dashboardService', function ($http) {
-  var getUserData = function () {
-    $http({
-      method : 'GET',
-      url : '/api/dashboard'
-    })
-  }
-})
+(function() {
+  'use strict';
 
-//////////////////////////////////////////////
-// .factory('browseService', function ($http) {
-//     var browse = function (region) {
-//       $http({
-//         method: 'GET',
-//         url: '/api/browse/:'+ region
-//       });
-//     };
-//
-//     return {browse: browse};
-//   });
+  angular.module('like.service', [])
+  .factory('authService', function ($http) {
+    var login = function (userObj) {
+     $http({
+      method: 'post',
+      url: '/api/login',
+      data: userObj
+     });
+    };
+    return {login: login};
+  })
+
+  .factory('dashboardService', function ($http) {
+    var getUserData = function () {
+      $http({
+        method: 'GET',
+        url: '/api/dashboard'
+      });
+    };
+  })
+
+  .factory('browseService', function ($http) {
+    var getUsers = function (region) {
+      $http({
+        method: 'GET',
+        url: '/api/browse/'
+      });
+    };
+    return {getUsers: getUsers};
+  });
+})();
