3cf96e93db0e8348967e887c0c20a5958e3593f1
Yifeng Wang <yeefom@gmail.com>
2015-12-08 20:35:51 -0800

    Add getAllUsers

--- public/app/controllers/browse.js
+++ public/app/controllers/browse.js
@@ -1,12 +1,20 @@
-(function() {
+(function () {
   'use strict';
 
   angular.module('like.browse', [])
   .controller('browseCtrl', ['$scope', 'browseService', function ($scope, browseService) {
     $scope.users = {};
 
-    $scope.getUsers = function () {
-      $scope.users = browseService.getUsers();
+    var getAllUsers = function () {
+      browseService.getAllUsers()
+      .then(function (users) {
+        $scope.users = users;
+      })
+      .catch(function (err) {
+        console.error('BROWSE — GET USERS ERRORS:', err);
+      });
     };
+
+    getAllUsers();
   }]);
 })();
