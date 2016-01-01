a8ae1d69edc16a2804101c6a982f25db7f34d219
Yifeng Wang <yeefom@gmail.com>
2015-12-31 19:59:32 -0800

    Modularize database models

--- server/Utilities/update.js
+++ server/Utilities/update.js
@@ -1,13 +1,14 @@
-var db = require('../models/schema');
+var db = require("../models");
 var util = require('./utilities');
 
 module.exports.username = function (id, username) {
   return db.Profile.update({ username : username},
                         { where : { id : id}})
                 .then(function() {
-                  console.log('update arguments', arguments)
-                })
-}
+                  console.log('update arguments', arguments);
+                });
+};
+
 module.exports.password  = function (id, password) {
   var username;
   // password = JSON.parse(password);
@@ -21,26 +22,28 @@ module.exports.password  = function (id, password) {
             if (correct) {
               return util.hashPassword(username, password.new)
                          .then(function(hash){
-                           console.log('new hash', hash)
-                           db.Profile.update({ password : hash },{ where :{ id : id }})
-                         })
+                           console.log('new hash', hash);
+                           db.Profile.update({ password : hash },{ where :{ id : id }});
+                         });
             } else {
               throw new Error('Error, incorrect password');
             }
-          })
+          });
 
-}
+};
+
 module.exports.firstName = function(id, firstName) {
   return db.Profile.update({ firstName : firstName},
                         { where : { id : id}});
+};
 
-}
 module.exports.lastName = function (id, lastName) {
   return db.Profile.update({ lastName : lastName},
                         { where : { id : id}});
 
-}
+};
+
 module.exports.email = function (id, email) {
   return db.Profile.update({ email : email},
                         { where : { id : id}});
-}
+};
